ed5076551e6faf26715ae83be9406337a6e31066
Siddharth190421 <siddharth.d@testriq.com>
2023-03-01 17:37:48 +0530

    Small update

--- cypress/e2e/pin.cy.js
+++ cypress/e2e/pin.cy.js
@@ -17,7 +17,7 @@ describe('Pins', ()=>{
         cy.contains('.red','Log in').click({force:true})
         cy.wait(4000)
         cy.get('input').type('Nirvana {enter}')
-        cy.wait(4000)
+        cy.wait(5000)
         // cy.get('[style="top: 0px; left: 0px; transform: translateX(252px) translateY(0px); width: 252px; height: 373px;"] > [style="padding-bottom: 16px; padding-left: 8px; padding-right: 8px;"] > [data-test-id="pin"] > [data-test-id="pinRepPresentation"] > .Jea > [aria-hidden="false"] > .sLG.zI7 > [style="height: 100%;"] > .Wk9 > .Pj7 > [data-test-id="pinrep-image"] > .KS5 > .ujU > :nth-child(1) > [data-test-id="non-story-pin-image"] > .XiG > .hCL').click()
 
     })
